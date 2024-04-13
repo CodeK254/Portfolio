78cd4f629371b232b6e6d9ff53e64df6a77e32bf
CodeK254 <codekarma1003@gmail.com>
2024-04-13 15:35:48 +0300

    Commit 0.0.9

--- lib/app/views/home/home_controller.dart
+++ lib/app/views/home/home_controller.dart
@@ -24,7 +24,7 @@ class HomeController extends GetxController{
     {"label": "Contact", "url": "/contact"},
   ];
 
-  RxInt selected = 2.obs;
+  RxInt selected = 0.obs;
 
   List<Color> colors = [
     Colors.black,
